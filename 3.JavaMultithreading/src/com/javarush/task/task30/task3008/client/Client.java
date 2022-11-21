9487c1593abadfe56540da957b1794603aa52782
Andrew Chuev <chuev.dev@gmail.com>
2022-11-21 19:16:33 +0300

    Большая задача Чат. добавлены методы в класс Client и SocketThread

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 public class Client {
 
@@ -8,7 +13,39 @@ public class Client {
 
     protected Connection connection;
 
-    public class SocketThread extends Thread {
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Введите адрес сервера:");
+        return ConsoleHelper.readString();
+    }
 
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Введите порт:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Введите имя пользователя:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected void sendTextMessage(String text) {
+        Message message = new Message(MessageType.TEXT, text);
+        try {
+            connection.send(message);
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Ошибка при отправке сообщения");
+            clientConnected = false;
+        }
+    }
+
+    public static class SocketThread extends Thread {
+
+        protected static SocketThread getSocketThread() {
+            return new SocketThread();
+        }
     }
 }
