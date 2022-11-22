dabb78b1bbd3fa53686e0855e34bee7e093c9bd7
Andrew Chuev <chuev.dev@gmail.com>
2022-11-22 12:55:47 +0300

    Большая задача Чат. Реализован BotClient

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/StringUtils.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/StringUtils.java
@@ -6,7 +6,7 @@ public class StringUtils {
 
     public static String encodeToWindows1251(String text) {
         try {
-            return new String(text.getBytes(), "windows-1251");
+            return new String(text.getBytes("UTF-8"), "windows-1251");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,10 @@
 package com.javarush.task.task30.task3008.client;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class BotClient extends Client {
 
     public static void main(String[] args) {
@@ -24,6 +29,64 @@ public class BotClient extends Client {
 
     public class BotSocketThread extends Client.SocketThread {
 
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String text) {
+            if (text.contains(":")) {
+                super.processIncomingMessage(text);
+                String[] splitText = text.split(":");
+                String name = splitText[0].trim();
+                String data = splitText[1].trim();
+
+                DateCommand command = DateCommand.getDateCommand(data);
+                if (command != DateCommand.UNSPECIFIED) {
+                    Calendar calendar = new GregorianCalendar();
+                    SimpleDateFormat formatter = new SimpleDateFormat(command.getFormat());
+                    String message = "Информация для " + name
+                            + ": " + formatter.format(calendar.getTime());
+                    super.processIncomingMessage(message);
+                }
+            }
+        }
+
+
+
+        private enum DateCommand {
+            DATE("дата", "d.MM.YYYY"),
+            DAY("день", "d"),
+            MONTH("месяц", "MMMM"),
+            YEAR("год", "YYYY"),
+            TIME("время", "H:mm:ss"),
+            HOUR("час", "H"),
+            MINUTES("минуты", "m"),
+            SECONDS("секунды", "s"),
+            UNSPECIFIED("", "");
+
+            private final String command;
+            private final String format;
+
+            DateCommand(String command, String format) {
+                this.command = command;
+                this.format = format;
+            }
+
+            public String getFormat() {
+                return format;
+            }
+
+            private static DateCommand getDateCommand(String textCommand) {
+                for (DateCommand dateCommand : values()) {
+                    if (dateCommand.command.equals(textCommand)) {
+                        return dateCommand;
+                    }
+                }
+                return UNSPECIFIED;
+            }
+        }
     }
 }
