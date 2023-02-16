5380f29e221c20e72fd3a504b0011138c081a35e
Andrew Chuev <chuev.dev@gmail.com>
2023-02-16 15:57:03 +0300

    Сокращатель ссылок (Shortener) №3310
    Решена задача 3310/9

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import com.javarush.task.task33.task3310.ExceptionHandler;
+import com.javarush.task.task33.task3310.Helper;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileBucket {
+
+    private Path path;
+
+    public FileBucket() {
+        try {
+            this.path = Files.createTempFile(Helper.generateRandomString(), null);
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+            path.toFile().deleteOnExit();
+        } catch (IOException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public long getFileSize() {
+        try {
+            return Files.size(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void putEntry(Entry entry) {
+        Entry current = getEntry();
+
+        while (current != null && current.next != null) {
+            current = current.next;
+        }
+
+        try (ObjectOutputStream os = new ObjectOutputStream(Files.newOutputStream(path))){
+            os.writeObject(entry);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Entry getEntry() {
+        if (getFileSize() == 0L)
+            return null;
+
+        try (ObjectInputStream objectIS = new ObjectInputStream(Files.newInputStream(path))){
+            return (Entry) objectIS.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void remove() {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
