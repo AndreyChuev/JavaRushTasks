b8a8bcf9c000e899c68a001bf4231b8b293fb1b5
Andrew Chuev <chuev.dev@gmail.com>
2023-02-19 10:37:21 +0300

    Сокращатель ссылок (Shortener) №3310-14
    Задание выполнено

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/tests/FunctionalTest.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task33.task3310.tests;
+
+
+import com.javarush.task.task33.task3310.Shortener;
+import com.javarush.task.task33.task3310.strategy.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FunctionalTest {
+
+    public void testStorage(Shortener shortener) {
+        List<String> strings = List.of("testString", "testStringUni", "testString");
+
+        List<Long> ids = strings.stream()
+                .map(shortener::getId)
+                .toList();
+
+
+        Assert.assertNotEquals(ids.get(1), ids.get(0));
+        Assert.assertNotEquals(ids.get(1), ids.get(2));
+
+        Assert.assertEquals(ids.get(0), ids.get(2));
+
+        List<String> storageStrings = ids.stream()
+                .map(shortener::getString)
+                .toList();
+
+        Assert.assertEquals(strings, storageStrings);
+    }
+
+    @Test
+    public void testHashMapStorageStrategy() {
+        testStorage(new Shortener(new HashMapStorageStrategy()));
+    }
+
+    @Test
+    public void testOurHashMapStorageStrategy() {
+        testStorage(new Shortener(new OurHashBiMapStorageStrategy()));
+    }
+
+    @Test
+    public void testFileStorageStrategy() {
+        testStorage(new Shortener(new FileStorageStrategy()));
+    }
+
+    @Test
+    public void testHashBiMapStorageStrategy() {
+        testStorage(new Shortener(new HashBiMapStorageStrategy()));
+    }
+
+    @Test
+    public void testDualHashBidiMapStorageStrategy() {
+        testStorage(new Shortener(new DualHashBidiMapStorageStrategy()));
+    }
+
+    @Test
+    public void testOurHashBiMapStorageStrategy() {
+        testStorage(new Shortener(new OurHashBiMapStorageStrategy()));
+    }
+
+}
