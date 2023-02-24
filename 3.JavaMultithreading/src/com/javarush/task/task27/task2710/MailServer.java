e945e12494887aa6768c6338734ba59e2c151703
Andrew Chuev <chuev.dev@gmail.com>
2023-02-24 09:18:26 +0300

    Задача 2710: Исправлены методы, решена задача

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -10,8 +10,16 @@ public class MailServer implements Runnable {
     @Override
     public void run() {
         long startTime = System.currentTimeMillis();
-        //сделайте что-то тут - do something here
         String name = Thread.currentThread().getName();
+        synchronized (mail) {
+            while (mail.getText() == null) {
+                try {
+                    mail.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
         long endTime = System.currentTimeMillis();
         System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
     }
