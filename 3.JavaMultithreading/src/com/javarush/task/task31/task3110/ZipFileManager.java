b7ca008dd39fee575697663a01d1c2d5f4422b78
Andrew Chuev <chuev.dev@gmail.com>
2023-02-25 09:37:46 +0300

    Archiver: в классе ZipFileManager, реализован метод createZip

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
 
@@ -11,6 +17,21 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOS = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry entry = new ZipEntry(source.getFileName().toString());
+            zipOS.putNextEntry(entry);
 
+            try (InputStream fileIS = Files.newInputStream(source)){
+                byte[] buffer = new byte[1024];
+                while (fileIS.available() > 0) {
+                    int bytesRead = fileIS.read(buffer);
+                    zipOS.write(buffer, 0, bytesRead);
+                    Arrays.fill(buffer, (byte) 0);
+                }
+            } finally {
+                zipOS.closeEntry();
+            }
+
+        }
     }
 }
