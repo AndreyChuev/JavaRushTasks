7b9a06bf199c1160e58c615bc4537dcf70618646
Andrew Chuev <chuev.dev@gmail.com>
2023-03-02 09:05:02 +0300

    Archiver: автоматически обновлены классы, решена задача (4)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,6 +11,7 @@ public class Archiver {
     public static void main(String[] args) throws Exception {
         ZipFileManager zipFileManager = new ZipFileManager(requestPath("Archive path: "));
         zipFileManager.createZip(requestPath("File path: "));
+        new ExitCommand().execute();
     }
 
     private static Path requestPath(String msg) throws IOException {
