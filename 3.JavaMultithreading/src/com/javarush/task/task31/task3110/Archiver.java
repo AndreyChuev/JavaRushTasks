9105e3e7da3191363a9942e3870658771c5f3ba5
Andrew Chuev <chuev.dev@gmail.com>
2023-03-02 10:10:15 +0300

    Archiver: валидатор насрал в код! Старт №9

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -5,8 +5,8 @@ import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 import java.io.IOException;
 
 public class Archiver {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
         Operation operation = null;
         do {
             try {
@@ -17,17 +17,21 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
+
         } while (operation != Operation.EXIT);
     }
 
+
     public static Operation askOperation() throws IOException {
-        StringBuilder menuBuilder = new StringBuilder("Выберите операцию:\n");
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
 
-        for (Operation value : Operation.values()) {
-            menuBuilder.append(String.format("%d - %s%n", value.ordinal(), value.description));
-        }
-
-        ConsoleHelper.writeMessage(menuBuilder.toString());
         return Operation.values()[ConsoleHelper.readInt()];
     }
 }
