798178f456a8127ce5097d0176e95ccf25905fb1
Andrew Chuev <chuev.dev@gmail.com>
2023-03-24 11:27:42 +0300

    Archiver: №12 старт

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -9,9 +9,8 @@ import java.nio.file.Paths;
 public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь к архиву:");
-        String rawArchivePath = ConsoleHelper.readString();
-        Path archivePath = Paths.get(rawArchivePath);
-        return new ZipFileManager(archivePath);
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
