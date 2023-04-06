6a54421851d9081652e4f898fe95deaf420957f0
Andrew Chuev <chuev.dev@gmail.com>
2023-04-06 10:03:48 +0300

    3709: задача решена

--- 4.JavaCollections/src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
@@ -1,13 +1,22 @@
 package com.javarush.task.task37.task3709.connectors;
 
+import com.javarush.task.task37.task3709.security.SecurityChecker;
+import com.javarush.task.task37.task3709.security.SecurityCheckerImpl;
+
 public class SecurityProxyConnector implements Connector {
 
-    public SecurityProxyConnector(String s) {
+    private final SecurityChecker checker;
+    private final SimpleConnector connector;
 
+    public SecurityProxyConnector(String s) {
+        checker = new SecurityCheckerImpl();
+        connector = new SimpleConnector(s);
     }
 
     @Override
     public void connect() {
-
+        if (checker.performSecurityCheck()) {
+            connector.connect();
+        }
     }
 }
